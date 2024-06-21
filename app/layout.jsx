e93a81e8e79de0b66dde909e7ec0049e078edd75
MBichajlo <maciej.bichajlo.mb@gmail.com>
2024-06-21 14:18:32 +0200

    adding vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import Navbar from "@components/navbar";
 import Footer from "@components/footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 export const metadata = {
   title: "Wrocław oczami AI",
   description: "",
@@ -10,6 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang='pl'>
       <body className=''>
+        <Analytics />
+        <SpeedInsights />
         <Navbar />
         {children}
         <Footer />
